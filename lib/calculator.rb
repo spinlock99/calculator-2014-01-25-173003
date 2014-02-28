7d7f6f73bcd079b2447e98055b4efc5e34ba9cd9
Andrew Dixon <andrew.dixon@twitch.tv>
2014-02-28 15:27:03 -0800

    clean up

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,2 +1,32 @@
 class Calculator
+  attr_reader :expr
+
+  def expr=(expression)
+    if expression.empty?
+      expression = "0"
+    end
+    @expr = expression.split(",").map(&:to_i)
+  end
+
+  alias :initialize :expr=
+
+  def add
+    return @expr.reduce(:+)
+  end
+
+  def diff
+    if @expr.length < 2
+      raise Exception
+    end
+    return @expr.reduce(:-)
+  end
+
+  def prod
+    return @expr.reduce(:*)
+  end
+
+  def div
+    raise Exception if @expr.include?(0)
+    @expr.reduce(:/)
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,11 +2,81 @@ require 'spec_helper'
 require 'calculator'
 
 describe Calculator do
+  let(:expression) { "1,2" }
+  subject(:calculator) { Calculator.new(expression) }
+
   describe "new" do
-    it "should instantiate" do
-      lambda {
-        Calculator.new
-      }.should_not raise_exception
+    subject(:new) { Calculator.new(expression) }
+    specify { expect { new }.to_not raise_error }
+  end
+
+  describe ".expr" do
+    it { should respond_to(:expr) }
+    its(:expr) { should eq([1,2]) }
+    it "resets the expression" do
+      calculator.expr = "2,3"
+      calculator.expr.should eq([2,3])
+    end
+  end
+
+  describe ".add" do
+    it { should respond_to(:add) }
+    its(:add) { should eq(3) }
+
+    context " '' " do
+      let(:expression) { '' }
+      its(:add) { should eq(0) }
+    end
+
+    context "1" do
+      let(:expression) { '1' }
+      its(:add) { should eq(1) }
+    end
+
+    context "1,2,3" do
+      let(:expression) { '1,2,3' }
+      its(:add) { should eq(6) }
+    end
+
+    context "1,2,5,8" do
+      let(:expression) { '1,2,5,8' }
+      its(:add) { should eq(16) }
+    end
+  end
+
+  describe ".diff" do
+    it { should respond_to(:diff) }
+    its(:diff) { should eq(-1) }
+    context '1,0' do
+      let(:expression) { '1,0' }
+      its(:diff) { should eq(1) }
+    end
+    context 'less than 2 digit expression' do
+      let(:expression) { '5' }
+      specify { expect { calculator.diff }.to raise_exception }
+    end
+  end
+
+  describe ".prod" do
+    its(:prod) { should eq(2) }
+
+    context '0' do
+      let(:expression) { '0' }
+      its(:prod) { should eq(0) }
+    end
+  end
+
+  describe ".div" do
+    its(:div) { should eq(0) }
+
+    context "divide by zero" do
+      let(:expression) { "1,2,3,0,99,1" }
+      specify { expect { calculator.div }.to raise_exception }
+    end
+
+    context "3,2,1" do
+      let(:expression) { "3,2,1" }
+      its(:div) { should eq(1) }
     end
   end
 end
